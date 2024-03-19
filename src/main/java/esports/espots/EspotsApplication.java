0caea73e4ac4f28aa567ab3b9cf2573997d0c024
rayen <rayenlaghouan01@gmail.com>
2024-03-19 14:50:33 +0100

    F commit

---
+++ src/main/java/esports/espots/EspotsApplication.java
@@ -0,0 +1,13 @@
+package esports.espots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esports/espots/EspotsApplicationTests.java
@@ -0,0 +1,13 @@
+package esports.espots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
